c0a89dfab179d07a1e938e84b18dc0a0f96e4e0d
Carlos-Manoel-2005 <carlosmanoelbfm@gmail.com>
2026-05-08 09:39:17 -0300

    chore: finalizar configuracao da infraestrutura e ferramentas do Maven

---
+++ src/main/java/simplexmed/SimplexMedApplication.java
@@ -0,0 +1,13 @@
+package simplexmed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplexMedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplexMedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/SimplexMed/SimplexMedApplicationTests.java
@@ -0,0 +1,13 @@
+package SimplexMed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplexMedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
